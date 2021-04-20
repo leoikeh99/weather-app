2a2a73e4b7fb7f98d603fe5e5bcba16028b76f56
Ikeh Leonard <leoikeh99@gmail.com>
2021-04-20 17:21:49 +0100

    finished main and search components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { useState } from "react";
 import "./App.css";
+import Main from "./components/Main";
+import Search from "./components/Search";
 
 function App() {
-  return <div className="app">Hello world</div>;
+  const [woeid, setWoeid] = useState(null);
+  return (
+    <div className="app">
+      <Main woeid={woeid} setWoeid={setWoeid} />
+      <Search setWoeid={setWoeid} />
+    </div>
+  );
 }
 
 export default App;
